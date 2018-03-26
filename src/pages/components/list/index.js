3763b48b9b742f2df3178b1026c77c063446735a
CD Cabrera <ccabrera@redhat.com>
2018-03-26 14:28:59 -0400

    DEV Clean up

--- src/pages/components/list/index.js
+++ src/pages/components/list/index.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import Documentation from '@siteComponents/Documentation'
 import Example from '@siteComponents/Example'
-import List, {Docs} from '@components/List'
-import ListItem from '@components/ListItem'
+import List, {ListItem, Docs} from '@components/List'
 
 export default () => {
+  const primary = 'Lorem ipsum';
+  const secondary = 'secondary';
+
   return (
     <Documentation docs={Docs}>
-      <Example heading="List Example">
+      <Example heading="List with toggle">
         <List>
-          <ListItem>
+          <ListItem showToggle={true} primary={primary} secondary={secondary}>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
           </ListItem>
+          <ListItem showToggle={true} primary={primary} secondary={secondary}>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          </ListItem>
+        </List>
+      </Example>
+
+      <Example heading="List without toggle">
+        <List>
+          <ListItem showToggle={false} primary={primary} secondary={secondary}/>
+          <ListItem showToggle={false} primary={primary} secondary={secondary}/>
         </List>
       </Example>
     </Documentation>
--- src/patternfly/components/List/index.js
+++ src/patternfly/components/List/index.js
@@ -2,13 +2,27 @@ import React from 'react'
 import docs from './docs.md'
 import './styles.scss'
 
-export const Docs = docs
+export const Docs = docs;
 
-var __html = require('./component.html');
-var template = { __html: __html };
+export const ListItem = ({showToggle = false, primary, secondary, children}) => {
+  return (
+    <div className="pf-c-list__item">
+      <div className="pf-c-list__header">
+        <div className="pf-l-split">
+          {showToggle && <div className="pf-l-split__secondary"><i className="fas fa-chevron-down"/></div>}
+          <div className="pf-l-split__primary">{primary}</div>
+          <div className="pf-l-split__secondary">{secondary}</div>
+        </div>
+      </div>
+      {children && <div className="pf-c-list_body">{children}</div>}
+    </div>
+  )
+};
 
-export default () => {
-    return (
-        <div dangerouslySetInnerHTML={template} ></div>
-    )
-}
+export default ({children, className = ''}) => {
+  return (
+    <div className={`pf-c-list ${className}`}>
+     {children}
+    </div>
+  )
+};
