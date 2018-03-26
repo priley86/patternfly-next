64f7064aba5425791493b63fc115fb6b22d45948
CD Cabrera <ccabrera@redhat.com>
2018-03-26 16:51:34 -0400

    Notes

--- src/patternfly/components/List/index.js
+++ src/patternfly/components/List/index.js
@@ -4,14 +4,18 @@ import './styles.scss'
 
 export const Docs = docs;
 
+/**
+ * Notes:
+ * * how much should we simplify jsx / html? how simple compared to handlebars?
+ * * should we add an IconButton component in PF?
+ * * prem pixel shadows conversion
+ * * do we need an expandable component? where is it going to be used, reused
+ * * button (w/ icon style) as just a link
+ */
 export const ListItem = ({showToggle = false, isExpanded = false, primary, secondary, children}) => {
   return (
     <div className={`pf-c-list-item ${showToggle && 'pf-is-expanded'}`}>
-
-
       <div className="pf-c-list-item__header">
-
-
         <div className="pf-l-split">
           {showToggle && (
             <div className="pf-l-split__secondary">
@@ -24,12 +28,26 @@ export const ListItem = ({showToggle = false, isExpanded = false, primary, secon
           <div className="pf-l-split__secondary">{secondary}</div>
         </div>
       </div>
-
-
       {children && isExpanded && <div className="pf-c-list-item__body">{children}</div>}
     </div>
   )
 };
+
+/*
+export const altListItem = ({children, className = ''}) => {
+  if () {
+    return (simple);
+  } else if () {
+    return (simple);
+  }
+
+  return (
+    <div className={`pf-c-list-item ${className}`}>
+        {children}
+    </div>
+  )
+};
+*/
 
 export default ({children, className = ''}) => {
   return (
